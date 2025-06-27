4e6df245fd96b71950b8c6869ec377448d1fd2bd
Migoooooooooo <Mina.Bekhit@triosstudent.com>
2025-06-27 00:10:54 -0400

    Save work before pulling

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component"; // Adjust the path as necessary
+import { ReservationsComponent } from "./reservations/reservations.component"; // Adjust the path as necessary
+import { ProductRepository } from "./model/product.repository";
+import { ReservationRepository } from "./model/reservation.repository";
+
+@NgModule({
+  declarations: [
+    AppComponent,ReservationsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    // Add any other necessary imports
+  ],
+  providers: [
+    ProductRepository,
+    ReservationRepository,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
